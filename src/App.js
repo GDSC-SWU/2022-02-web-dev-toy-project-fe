22c9e0943885a5f37cb31a7029765bf61eebdb14
nusuy <nsuy@gmail.com>
2023-01-02 02:50:48 +0900

    feat: Add LoginPage (#1)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import styles from "./App.module.css";
+import LoginPage from "./pages/LoginPage.js";
 
 function App() {
-  return <div className={styles.App}></div>;
+  return (
+    <div className={styles.App}>
+      <LoginPage />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/LoginPage.js
@@ -0,0 +1,10 @@
+import React from "react";
+import GoogleLogin from "../components/GoogleLogin.js";
+
+export default function LoginPage() {
+  return (
+    <React.Fragment>
+      <GoogleLogin />
+    </React.Fragment>
+  );
+}
