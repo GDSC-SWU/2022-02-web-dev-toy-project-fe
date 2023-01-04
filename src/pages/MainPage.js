543c382b4fb47bdabb83e932a40f14560b2f410b
nusuy <nsuy@gmail.com>
2023-01-04 16:19:21 +0900

    feat: Add MainPage (#3)

---
+++ src/pages/MainPage.js
@@ -0,0 +1,60 @@
+import styles from "./MainPage.module.css";
+
+function MainPage() {
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.content}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
+        tristique leo vitae nisl placerat, ut facilisis magna rutrum. Curabitur
+        mollis tortor nisl, ac ultrices enim facilisis vitae. Donec nec dolor
+        sit amet turpis vulputate ornare. Sed lacus velit, interdum ac posuere
+        et, pulvinar sit amet tortor. Nunc eget magna ex. Sed volutpat id ante
+        in posuere. Integer quis velit ac odio iaculis interdum quis a felis.
+        Integer mollis nulla leo, a maximus felis commodo vitae. Sed vel massa
+        dui. Phasellus tempus risus vitae ex scelerisque, vitae tincidunt ex
+        ornare. Cras nec eros dolor.
+        <br />
+        <br />
+        Maecenas maximus bibendum sem id eleifend. Etiam iaculis massa felis, in
+        finibus sapien maximus semper. Praesent facilisis, ligula et convallis
+        aliquet, ante leo volutpat nisl, ut bibendum ex nisl nec libero. Nulla
+        vel mi efficitur, volutpat massa sed, maximus lorem. Donec eget sem sit
+        amet tortor aliquam volutpat non quis ante. Aenean non mattis risus.
+        Donec maximus purus sed sapien dignissim, vel tempor est commodo.
+        Aliquam erat volutpat. Morbi elementum, tellus quis volutpat sagittis,
+        massa diam commodo neque, ac eleifend nibh nisi sit amet lorem. Nam eu
+        ante vitae sapien molestie mollis. Phasellus sodales, odio consequat
+        venenatis tristique, nibh orci feugiat ligula, ut rutrum ligula tortor
+        sit.
+        <br />
+        <br />
+        Maecenas maximus bibendum sem id eleifend. Etiam iaculis massa felis, in
+        finibus sapien maximus semper. Praesent facilisis, ligula et convallis
+        aliquet, ante leo volutpat nisl, ut bibendum ex nisl nec libero. Nulla
+        vel mi efficitur, volutpat massa sed, maximus lorem. Donec eget sem sit
+        amet tortor aliquam volutpat non quis ante. Aenean non mattis risus.
+        Donec maximus purus sed sapien dignissim, vel tempor est commodo.
+        Aliquam erat volutpat. Morbi elementum, tellus quis volutpat sagittis,
+        massa diam commodo neque, ac eleifend nibh nisi sit amet lorem. Nam eu
+        ante vitae sapien molestie mollis. Phasellus sodales, odio consequat
+        venenatis tristique, nibh orci feugiat ligula, ut rutrum ligula tortor
+        sit.
+        <br />
+        <br />
+        Maecenas maximus bibendum sem id eleifend. Etiam iaculis massa felis, in
+        finibus sapien maximus semper. Praesent facilisis, ligula et convallis
+        aliquet, ante leo volutpat nisl, ut bibendum ex nisl nec libero. Nulla
+        vel mi efficitur, volutpat massa sed, maximus lorem. Donec eget sem sit
+        amet tortor aliquam volutpat non quis ante. Aenean non mattis risus.
+        Donec maximus purus sed sapien dignissim, vel tempor est commodo.
+        Aliquam erat volutpat. Morbi elementum, tellus quis volutpat sagittis,
+        massa diam commodo neque, ac eleifend nibh nisi sit amet lorem. Nam eu
+        ante vitae sapien molestie mollis. Phasellus sodales, odio consequat
+        venenatis tristique, nibh orci feugiat ligula, ut rutrum ligula tortor
+        sit.
+      </div>
+    </div>
+  );
+}
+
+export default MainPage;
