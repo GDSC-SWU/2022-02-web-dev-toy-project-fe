7cb3d7b03ef6091ea35ae77ca01204810ee8ea08
nusuy <nsuy@gmail.com>
2023-01-04 16:18:53 +0900

    feat: Add Router (#3)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import styles from "./App.module.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Nav from "./components/Nav";
+import MainPage from "./pages/MainPage";
 
 function App() {
-  return <div className={styles.App}></div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.header}></div>
+      <div className={styles.nav}>
+        <Nav />
+      </div>
+      <div className={styles.content}>
+        <Router>
+          <Routes>
+            {/* NAV 컴포넌트에 설정한 href로 라우팅 */}
+            <Route path="/" element={<MainPage />} />
+          </Routes>
+        </Router>
+      </div>
+      <div className={styles.footer}></div>
+    </div>
+  );
 }
 
 export default App;
