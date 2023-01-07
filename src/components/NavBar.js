05619e1401d8fbace808f7194a7c594e81482c58
nusuy <nsuy@gmail.com>
2023-01-07 19:10:57 +0900

    feat: Add icons on NavBar (#3)
    - HomeIcon, NewPostIcon, ListIcon

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,96 +1,27 @@
 import React from "react";
-import { useDispatch, useSelector } from "react-redux";
 import styles from "./NavBar.module.css";
-import setUserInfo from "../store/setUserInfo";
+import { ReactComponent as HomeIcon } from "../assets/images/home_icon.svg";
+import { ReactComponent as ListIcon } from "../assets/images/home_icon_list.svg";
+import { ReactComponent as NewPostIcon } from "../assets/images/new_post_icon.svg";
+import { Link } from "react-router-dom";
 
 function Nav() {
-  const dispatch = useDispatch();
-  const state = useSelector((state) => state);
-
-  // 테스트용
-  const onSignInClick = () => {
-    console.log("SignInClick");
-    setUserInfo(dispatch, "anonymous", "1234");
-  };
-  const onSignOutClick = () => {
-    console.log("SignOutClick");
-    if (window.confirm("로그아웃 하시겠습니까?")) {
-      setUserInfo(dispatch);
-    }
-  };
-
   return (
     <div className={styles.container}>
-      <div className={styles.title}>
-        {/* title 클릭 시 메인으로 이동 */}
-        <a href="/" className={styles.titleText}>
-          TITLE
-        </a>
+      <div className={styles.homeWrapper}>
+        <Link to="/">
+          <HomeIcon className={styles.homeIcon} />
+        </Link>
       </div>
-      {/* 각 메뉴 선택 시 이동 */}
-      <ul className={styles.menu}>
-        <li>
-          <a
-            href="#section"
-            onClick={() => {
-              console.log("MENU1 Cliked");
-            }}
-            className={styles.menuItem}
-          >
-            MENU1
-          </a>
-        </li>
-        <li>
-          <a
-            href="#section"
-            onClick={() => {
-              console.log("MENU2 Cliked");
-            }}
-            className={styles.menuItem}
-          >
-            MENU2
-          </a>
-        </li>
-        <li>
-          <a
-            href="#section"
-            onClick={() => {
-              console.log("MENU3 Cliked");
-            }}
-            className={styles.menuItem}
-          >
-            MENU3
-          </a>
-        </li>
-        <li>
-          <a
-            href="#section"
-            onClick={() => {
-              console.log("MENU4 Cliked");
-            }}
-            className={styles.menuItem}
-          >
-            MENU4
-          </a>
-        </li>
-      </ul>
-      {/* 유저 정보 (로그인 / 마이페이지 + 로그아웃) */}
-      <div className={styles.user}>
-        {state.userName !== null ? (
-          // My Page로 연결
-          <a
-            href="#section"
-            onClick={onSignOutClick}
-            className={styles.nickName}
-          >
-            {state.userName} 님
-          </a>
-        ) : (
-          // 회원가입 페이지로 연결
-          <a href="#section" onClick={onSignInClick} className={styles.signIn}>
-            Sign In
-          </a>
-        )}
+      <div className={styles.newPostWrapper}>
+        <Link to="/">
+          <NewPostIcon className={styles.newPostIcon} />
+        </Link>
+      </div>
+      <div className={styles.listWrapper}>
+        <Link to="/">
+          <ListIcon className={styles.listIcon} />
+        </Link>
       </div>
     </div>
   );
