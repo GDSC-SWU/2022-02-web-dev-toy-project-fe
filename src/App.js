106e0640498c99a416ffdec8b3b23bf16de78f5a
nusuy <nsuy@gmail.com>
2023-01-07 18:41:28 +0900

    fix: Remove BrowserRouter on App.js (#3)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import styles from "./App.module.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import MainPage from "./pages/MainPage";
 
@@ -11,12 +11,10 @@ function App() {
         <NavBar />
       </div>
       <div className={styles.content}>
-        <Router>
-          <Routes>
-            {/* NAV 컴포넌트에 설정한 href로 라우팅 */}
-            <Route path="/" element={<MainPage />} />
-          </Routes>
-        </Router>
+        <Routes>
+          {/* NAV 컴포넌트에 설정한 href로 라우팅 */}
+          <Route path="/" element={<MainPage />} />
+        </Routes>
       </div>
       <div className={styles.footer}></div>
     </div>
