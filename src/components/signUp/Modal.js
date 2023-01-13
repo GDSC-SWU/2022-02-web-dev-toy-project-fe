e2d4fcbccb52b7faa3ec0acc58bafff80e140f66
yeonsu <drawingbonny@gmail.com>
2023-01-13 22:30:13 +0900

    fix(css): Modify font size (#8)

--- src/components/signUp/Modal.js
+++ src/components/signUp/Modal.js
@@ -1,37 +1,37 @@
-import { useState } from 'react';
-import ModalBasic from './ModalBasic';
-import styled from 'styled-components';
+import { useState } from "react";
+import ModalBasic from "./ModalBasic";
+import styled from "styled-components";
 
 // 모달을 노출하는 페이지
 function Modal() {
-    // 모달창 노출 여부 state
-    const [modalOpen, setModalOpen] = useState(false);
+  // 모달창 노출 여부 state
+  const [modalOpen, setModalOpen] = useState(false);
 
-    // 모달창 노출
-    const showModal = () => {
-        setModalOpen(true);
-    };
+  // 모달창 노출
+  const showModal = () => {
+    setModalOpen(true);
+  };
 
-    const ModalBg = styled.div`
-    color: #AFAFAF;
+  const ModalBg = styled.div`
+    color: #afafaf;
     width: 10.8rem;
     border-radius: 32px;
     margin: 0 auto;
     margin-top: 2rem;
-    font-size: 10px;
-    border: 1px solid #C9C9C9;
+    font-size: 0.75rem;
+    border: 1px solid #c9c9c9;
     padding: 0.5rem 2rem;
-`;
+    text-align: center;
+  `;
 
-
-    return (
-        <div>
-            <button onClick={showModal}>
-                <ModalBg>로그인에 어려움이 있나요?</ModalBg>
-                </button>
-            {modalOpen && <ModalBasic setModalOpen={setModalOpen} />}
-        </div>
-    );
+  return (
+    <div>
+      <button onClick={showModal}>
+        <ModalBg>로그인에 어려움이 있나요?</ModalBg>
+      </button>
+      {modalOpen && <ModalBasic setModalOpen={setModalOpen} />}
+    </div>
+  );
 }
 
 export default Modal;
