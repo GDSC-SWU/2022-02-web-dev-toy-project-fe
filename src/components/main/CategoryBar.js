4d5eae604c9ad5872eb55dd0c677a757b0904f7a
nusuy <nsuy@gmail.com>
2023-01-16 02:41:16 +0900

    feat: Add current state props on CategoryBar (#11)

--- src/components/main/CategoryBar.js
+++ src/components/main/CategoryBar.js
@@ -2,12 +2,25 @@ import styles from "./CategoryBar.module.css";
 import category_data from "../../data/category.json";
 import CategoryButton from "./CategoryButton";
 
-function CategoryBar() {
+function CategoryBar({ currentCategory }) {
   const render = () => {
     const result = [];
+    result.push(
+      <CategoryButton
+        key="all"
+        title="전체"
+        icon={null}
+        isSelected={currentCategory === "전체"}
+      />
+    );
     category_data.data.map((e, idx) => {
       result.push(
-        <CategoryButton key={`${idx}`} title={e.title} icon={e.icon} />
+        <CategoryButton
+          key={`${idx}`}
+          title={e.title}
+          icon={currentCategory === e.title ? e.icon[1] : e.icon[0]}
+          isSelected={currentCategory === e.title}
+        />
       );
 
       return result;
