d949283b1cfb13b62a99513d5f254ec93c00260d
nusuy <nsuy@gmail.com>
2023-01-16 02:55:26 +0900

    fix: Remove unused sample image map data (#11)

--- src/components/main/MainMap.js
+++ src/components/main/MainMap.js
@@ -1,13 +1,9 @@
 import styles from "./MainMap.module.css";
 
 function MainMap() {
-  const MAP_PATH = require("../../data/map_sample.png");
-
   return (
     <div className={styles.container}>
-      <div className={styles.map}>
-        <img src={MAP_PATH} alt="map" />
-      </div>
+      <div className={styles.map}></div>
       <div className={styles.itemContainer}></div>
     </div>
   );
