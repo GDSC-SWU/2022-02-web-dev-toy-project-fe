058f8aae27f75c2a119cc4ee326c309881cea455
nusuy <nsuy@gmail.com>
2023-01-20 23:11:31 +0900

    fix: Remove clusterer on MainMap (#11)

--- src/components/main/MainMap.js
+++ src/components/main/MainMap.js
@@ -1,12 +1,13 @@
 import React, { useEffect, useState, useRef } from "react";
-import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
+import { GoogleMap, useJsApiLoader, MarkerF } from "@react-google-maps/api";
 import styles from "./MainMap.module.css";
 import { ReactComponent as CurrentLoc } from "../../assets/images/currentLoc_icon.svg";
 import { ReactComponent as CurrentLocSelected } from "../../assets/images/currentLoc_icon_selected.svg";
 import { useLocation } from "react-router-dom";
 import sample_data from "../../data/items_sample_data.json";
 import swu_place_data_sample from "../../data/swu_place_data_sample.json";
-import { MarkerClusterer } from "@googlemaps/markerclusterer";
+import blue from "../../assets/images/clusterer_images/blueCircle.svg";
+import black from "../../assets/images/clusterer_images/blackCircle.svg";
 
 // google map options
 const API_KEY = process.env.REACT_APP_GOOGLE_MAP_API_KEY;
@@ -20,6 +21,7 @@ const mapOptions = {
   streetViewControl: false,
   zoomControl: false,
   zoom: 18,
+  minZoom: 16,
 };
 
 let currentPath = "";
@@ -37,6 +39,8 @@ function MainMap({ isFound, currentCategory }) {
   }, [location]);
 
   const mapRef = useRef(null);
+  const clustererRef = useRef(null);
+  const markerRef = useRef(null);
   const [center, setCenter] = useState({
     // (기본) 서울여자대학교 좌표
     lat: 37.62814,
@@ -46,37 +50,15 @@ function MainMap({ isFound, currentCategory }) {
   const [isCurrent, setIsCurrent] = useState(false);
   const { geolocation } = navigator;
 
-  // cluster 초기화
-  const cluster_data = [];
-  swu_place_data_sample.data.map((item) => {
-    cluster_data.push({
-      place: item.place,
-      loc: { lat: item.lat, lng: item.lng },
-      count: 0,
-      posts: [],
-    });
-
-    return cluster_data;
-  });
-  let temp = JSON.parse(JSON.stringify(cluster_data));
-  const [cluster, setCluster] = useState(temp);
-  const [currentMarkers, setCurrentMarkers] = useState(null);
-  let markers = [];
-
   // google map
   const { isLoaded } = useJsApiLoader({
     id: "google-map-script",
     googleMapsApiKey: API_KEY,
   });
   const [map, setMap] = React.useState(null);
-  const onLoad = React.useCallback(
-    function callback(map) {
-      console.log(map);
-      setMap(map);
-      addMarkers(map);
-    },
-    [addMarkers]
-  );
+  const onLoad = React.useCallback(function callback(map) {
+    setMap(map);
+  }, []);
   const onUnmount = React.useCallback(function callback(map) {
     setMap(null);
   }, []);
@@ -126,9 +108,43 @@ function MainMap({ isFound, currentCategory }) {
     );
   };
 
+  // clusterer
+  // cluster 초기화
+  const cluster_data = [];
+  swu_place_data_sample.data.map((item) => {
+    cluster_data.push({
+      place: item.place,
+      loc: { lat: item.lat, lng: item.lng },
+      count: 0,
+      posts: [],
+    });
+
+    return cluster_data;
+  });
+  let temp = JSON.parse(JSON.stringify(cluster_data));
+  const [markers, setMarkers] = useState(temp);
+
   useEffect(() => {
-    console.log("effect");
-    const result = cluster;
+    // search type이나 현재 카테고리가 변경될 경우 클러스터러 Repaint
+    console.log(clustererRef?.current);
+    console.log(markerRef?.current);
+    if (map) {
+      // hide markers
+      markers.length = 0;
+      for (let i = 0; i < markers.length; i++) {
+        markers[i].setMap(null);
+        setMarkers(null);
+      }
+      // delete markers
+      addMarkers(map);
+    }
+    setMap(null);
+    addMarkers();
+    clustererRef.current?.repaint();
+  }, [isFound, currentCategory]);
+
+  function addMarkers() {
+    const result = markers;
 
     result.map((item) => {
       const i = item;
@@ -138,8 +154,7 @@ function MainMap({ isFound, currentCategory }) {
       return result;
     });
 
-    setCluster(result);
-    console.log(cluster);
+    setMarkers(result);
 
     let flag; // 분류 위한 플래그
     if (isFound && currentCategory === "전체") {
@@ -152,7 +167,6 @@ function MainMap({ isFound, currentCategory }) {
       flag = 3;
     }
 
-    console.log(`flag: ${flag}`);
     switch (flag) {
       case 0: // 습득, 카테고리 전체
         // 게시물 장소에 따라 cluster state의 count 증가 + postId 배열에 추가
@@ -223,87 +237,9 @@ function MainMap({ isFound, currentCategory }) {
         break;
     }
 
-    setCluster(result);
-    console.log(map);
-    if (map) {
-      // hide markers
-      for (let i = 0; i < currentMarkers.length; i++) {
-        currentMarkers[i].setMap(null);
-        setCurrentMarkers(null);
-      }
-      // delete markers
-      markers = [];
-      addMarkers(map);
-    }
-  }, [isFound, currentCategory]);
+    setMarkers(result);
 
-  const newMarkers = () => {
-    cluster.map((item) => {
-      if (item.count > 0) {
-        const marker = new window.google.maps.Marker({
-          position: { lat: item.loc.lat, lng: item.loc.lng },
-          map,
-          icon: require(`../../assets/images/clusterer_images/${
-            isFound ? "found" : "lost"
-          }${item.count > 10 ? 10 : item.count}.svg`),
-          label: {
-            text: `${item.count}`,
-            color: "white",
-            fontSize: "15px",
-            fontWeight: "400",
-          },
-        });
-        markers.push(marker);
-
-        return marker;
-      }
-      console.log(markers);
-
-      return markers;
-    });
-
-    return markers;
-  };
-
-  function addMarkers(map) {
-    console.log("add");
-    console.log(cluster);
-
-    setCurrentMarkers(newMarkers());
-
-    function labelSum(markers) {
-      let result = 0;
-      markers.map((item) => {
-        result = result + parseInt(item.label.text);
-
-        return markers;
-      });
-      return result;
-    }
-
-    const renderer = {
-      render: ({ count, position, markers }) =>
-        new window.google.maps.Marker({
-          position,
-          map,
-          icon: require(`../../assets/images/clusterer_images/${
-            isFound ? "found" : "lost"
-          }${labelSum(markers) > 10 ? 10 : labelSum(markers)}.svg`),
-          label: {
-            text: `${labelSum(markers)}`,
-            color: "white",
-            fontSize: "15px",
-            fontWeight: "400",
-            zIndex: Number(window.google.maps.Marker.MAX_ZINDEX) + count,
-          },
-        }),
-    };
-
-    new MarkerClusterer({
-      map,
-      markers,
-      renderer,
-    });
+    console.log(markers);
   }
 
   return (
@@ -318,7 +254,24 @@ function MainMap({ isFound, currentCategory }) {
             options={mapOptions}
             ref={mapRef}
             onDragStart={onMapDragHandle}
-          ></GoogleMap>
+          >
+            {markers.map((marker, i) => (
+              <MarkerF
+                className={styles.marker}
+                onLoad={(marker) => {
+                  markerRef.current = marker;
+                }}
+                key={i}
+                position={{ lat: marker.loc.lat, lng: marker.loc.lng }}
+                noClustererRedraw={false}
+                icon={{
+                  url: `${isFound ? blue : black}`,
+                  height: 44,
+                  width: 44,
+                }}
+              />
+            ))}
+          </GoogleMap>
         )}
       </div>
       <div className={styles.currentLocContainer}>
@@ -335,9 +288,3 @@ function MainMap({ isFound, currentCategory }) {
 }
 
 export default React.memo(MainMap);
-
-const typeChange = new CustomEvent("typeChange", {
-  detail: {
-    name: "change-type clicked",
-  },
-});
