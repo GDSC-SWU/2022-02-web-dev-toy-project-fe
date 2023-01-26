d7a0a62382b1e4b473cc14cfdfa71b62fec32135
nusuy <nsuy@gmail.com>
2023-01-27 04:06:15 +0900

    feat: Add Router for ListPage, SearchPage (#14)

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,9 @@
 import styles from "./HomePage.module.css";
 import { Routes, Route } from "react-router-dom";
 import MainPage from "./MainPage";
-import NavBar from "../components/NavBar";
+import NavBar from "../components/navigation/NavBar";
+import ListPage from "./list/ListPage";
+import SearchPage from "./list/SearchPage";
 
 function HomePage() {
   return (
@@ -14,6 +16,8 @@ function HomePage() {
         <Routes>
           {/* NAV 컴포넌트에 설정한 href로 라우팅 */}
           <Route path="/" element={<MainPage />} />
+          <Route path="/list" element={<ListPage />} />
+          <Route path="/search" element={<SearchPage />} />
         </Routes>
       </div>
       <div className={styles.footer}></div>
