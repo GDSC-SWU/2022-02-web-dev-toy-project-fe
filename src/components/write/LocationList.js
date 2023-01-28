7a2329105f37fefce6f7813adaa0b28d69ace5ea
nusuy <nsuy@gmail.com>
2023-01-28 17:28:41 +0900

    feat: Add props on Location (#17)

--- src/components/write/LocationList.js
+++ src/components/write/LocationList.js
@@ -1,15 +1,19 @@
+import { useLocation } from "react-router-dom";
 import Location from "./Location";
 import styles from "./LocationList.module.css";
 
 function LocationList() {
+  const location = useLocation();
+  const currentDetailLoc = location.state.detail; // 이전 페이지에서 가져온 세부 위치 (아직 작성하지 않은 상태에서 넘어왔다면 null)
+
   return (
     <div className={styles.container}>
       <div className={styles.box}>
-        <Location />
-        <Location />
-        <Location />
-        <Location />
-        <Location />
+        <Location detail={currentDetailLoc} />
+        <Location detail={currentDetailLoc} />
+        <Location detail={currentDetailLoc} />
+        <Location detail={currentDetailLoc} />
+        <Location detail={currentDetailLoc} />
       </div>
     </div>
   );
