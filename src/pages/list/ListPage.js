df96eab5987d281694ca57ea0201d3baecd8bd3e
Yusun Choi <nsuy.ch@gmail.com>
2023-02-01 03:09:02 +0900

    feat: Modify ListPage (#20)
    - fix top bar

--- src/pages/list/ListPage.js
+++ src/pages/list/ListPage.js
@@ -15,41 +15,43 @@ function ListPage() {
 
   return (
     <div className={styles.container}>
-      <div className={styles.tabBarContainer}>
-        <div className={styles.tabWrapper}>
-          <ul className={styles.tab}>
-            <li
-              className={`${styles.tabLi} ${tab === 0 && styles.activeTab}`}
-              onClick={() => {
-                const tab = 0;
-                setTab(tab);
-              }}
-            >
-              찾아가세요
-            </li>
-            <li
-              className={`${styles.tabLi} ${tab === 1 && styles.activeTab}`}
-              onClick={() => {
-                const tab = 1;
-                setTab(tab);
-              }}
-            >
-              잃어버렸어요
-            </li>
-          </ul>
+      <div className={styles.topContainer}>
+        <div className={styles.tabBarContainer}>
+          <div className={styles.tabWrapper}>
+            <ul className={styles.tab}>
+              <li
+                className={`${styles.tabLi} ${tab === 0 && styles.activeTab}`}
+                onClick={() => {
+                  const tab = 0;
+                  setTab(tab);
+                }}
+              >
+                찾아가세요
+              </li>
+              <li
+                className={`${styles.tabLi} ${tab === 1 && styles.activeTab}`}
+                onClick={() => {
+                  const tab = 1;
+                  setTab(tab);
+                }}
+              >
+                잃어버렸어요
+              </li>
+            </ul>
+          </div>
+          <div className={styles.searchWrapper}>
+            <Link to="/home/search">
+              <SearchIcon width={22} height={22} fill={"#000000"} />
+            </Link>
+          </div>
         </div>
-        <div className={styles.searchWrapper}>
-          <Link to="/home/search">
-            <SearchIcon width={22} height={22} fill={"#000000"} />
-          </Link>
+        <div className={styles.border}>
+          <div
+            className={`${styles.borderActive} ${
+              tab === 0 ? styles.foundBorder : styles.lostBorder
+            }`}
+          ></div>
         </div>
-      </div>
-      <div className={styles.border}>
-        <div
-          className={`${styles.borderActive} ${
-            tab === 0 ? styles.foundBorder : styles.lostBorder
-          }`}
-        ></div>
       </div>
       <div className={styles.content}>{tabList[tab]}</div>
     </div>
