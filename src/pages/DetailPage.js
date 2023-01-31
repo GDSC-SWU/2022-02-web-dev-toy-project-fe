9636e744c806a85d7d0c85e52510320411419e11
nusuy <nsuy.ch@gmail.com>
2023-02-01 02:57:07 +0900

    feat: Add DetailPage (#20)
    - markup

--- src/pages/DetailPage.js
+++ src/pages/DetailPage.js
@@ -2,8 +2,62 @@ import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import styles from "./DetailPage.module.css";
 import Comment from "../components/Comment";
-//import data from "../data/samples/sample_data.json";
 import API from "../api/API";
+import foundCover from "../assets/images/postDetail/foundCover.png";
+import { ReactComponent as BackKey } from "../assets/images/back_Key.svg";
+import { ReactComponent as BellNone } from "../assets/images/postDetail/bell_none.svg";
+import { ReactComponent as Bell } from "../assets/images/postDetail/bell.svg";
+import { ReactComponent as Share } from "../assets/images/postDetail/share_post.svg";
+import { ReactComponent as Complete } from "../assets/images/postDetail/completed.svg";
+import { ReactComponent as Send } from "../assets/images/postDetail/send.svg";
+import { ReactComponent as MiddleDot } from "../assets/images/postDetail/middleDot.svg";
+import { ReactComponent as Loc } from "../assets/images/loc_icon.svg";
+import { ReactComponent as Profile } from "../assets/images/postDetail/profile_none.svg";
+import { ReactComponent as Headphones } from "../assets/images/category/headphones.svg";
+import { ReactComponent as Wallet } from "../assets/images/category/wallet.svg";
+import { ReactComponent as Clothes } from "../assets/images/category/clothes.svg";
+import { ReactComponent as Book } from "../assets/images/category/book.svg";
+//import data from "../data/samples/sample_data.json";
+
+const maincolor = getComputedStyle(document.documentElement).getPropertyValue(
+  "--main-color"
+);
+
+const Tag = ({ isTag, title }) => {
+  const enumChoice_icon = {
+    전자제품: <Headphones stroke={maincolor} />,
+    귀중품: <Wallet stroke={maincolor} />,
+    의류: <Clothes stroke={maincolor} />,
+    서적: <Book stroke={maincolor} />,
+  };
+
+  return (
+    <div
+      className={
+        title.includes("주인을")
+          ? `${styles.tagContainer} ${styles.foundTagContainer}`
+          : `${styles.tagContainer}`
+      }
+    >
+      <div className={styles.tagContent}>
+        {isTag && (
+          <div className={styles.tagIconWrapper}>{enumChoice_icon[title]}</div>
+        )}
+        <div className={styles.tagTitleWrapper}>
+          <span
+            className={
+              title.includes("주인을")
+                ? `${styles.tagTitle} ${styles.foundTagTitle}`
+                : `${styles.tagTitle}`
+            }
+          >
+            {title}
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 function DetailPage() {
   const { postid } = useParams();
@@ -13,58 +67,160 @@ function DetailPage() {
   useEffect(() => {
     const getPost = async () => {
       try {
-        const post = await API.get("/post", { params: { id: postid } });
-        setPost(post);
-        console.log(post);
+        const post = await API.get(`/post/${postid}`);
+        setPost(post.data);
+        console.log(post.data);
       } catch (err) {
         console.log(err);
       }
     };
 
-    getPost().then((res) => console.log(res));
+    getPost();
   }, []);
 
   return (
     <div className={styles.container}>
       <div className={styles.imageContainer}>
+        <img
+          className={styles.image}
+          src={require("../data/samples/sample2.png")}
+          alt="post image"
+        />
+        {post?.status === "true" && (
+          <div className={styles.coverWrapper}>
+            <img
+              className={styles.cover}
+              src={foundCover}
+              alt="This item has found its owner."
+            />
+          </div>
+        )}
         <div className={styles.topbarContainer}>
-          <div className={styles.prevIconWrapper}></div>
-          <div className={styles.notifiIconWrapper}></div>
-          <div className={styles.shareIconWrapper}></div>
+          <div className={styles.backIconWrapper}>
+            <BackKey />
+          </div>
+          <div className={styles.topbar_right}>
+            <div className={styles.notifiIconWrapper}>
+              <BellNone />
+            </div>
+            <div className={styles.shareIconWrapper}>
+              <Share />
+            </div>
+          </div>
         </div>
         <div className={styles.imageOrderContainer}></div>
       </div>
-      <div className={styles.content}>
+      <div className={styles.post}>
         <div className={styles.postInfoContainer}>
-          <div className={styles.profileIconWrapper}></div>
-          <div className={styles.writerWrapper}></div>
-          <div className={styles.publishDateWrapper}></div>
+          <div className={styles.profileIconWrapper}>
+            <Profile className={styles.profileIcon} />
+          </div>
+          <div className={styles.writerWrapper}>
+            <span className={styles.writer}>익명</span>
+          </div>
+          <div className={styles.publishDateWrapper}>
+            <span className={styles.publishDate}>01/18</span>
+            <span className={styles.publishDate}>20:41</span>
+          </div>
         </div>
         <div className={styles.borderLine}></div>
         <div className={styles.postContentContainer}>
-          <div className={styles.tagContainer}></div>
-          <div className={styles.titleWrapper}>
-            <span className={styles.title}>{post?.title}</span>
+          <div className={styles.tagBarContainer}>
+            <div className={styles.typeWrapper}>
+              {/*  <Tag
+                isTag={false}
+                title={post?.postStatus.includes("found") ? "습득" : "분실"}
+              /> */}
+              <Tag isTag={false} title={"습득"} />
+            </div>
+            <div className={styles.tagWrapper}>
+              {/*  <Tag
+                isTag={true}
+                title={post?.tag}
+              /> */}
+              <Tag isTag={true} title={"전자제품"} />
+            </div>
+            <div className={styles.statusWrapper}>
+              {/* post?.status === "true" && (
+                <Tag isTag={false} title={"주인을 찾았어요!"} />
+              ) */}
+              <Tag isTag={false} title={"주인을 찾았어요!"} />
+            </div>
           </div>
-          <div className={styles.locContainer}></div>
+          <div className={styles.titleWrapper}>
+            <span className={styles.title}>
+              {/*post?.title*/}버티컬 마우스 찾아가세요.
+            </span>
+          </div>
+          <div className={styles.locContainer}>
+            <div className={styles.locIconWrapper}>
+              <Loc className={styles.locIcon} />
+            </div>
+            <div className={styles.locWrapper}>
+              <span className={styles.loc}>{/*post?.place*/}제2과학관</span>
+            </div>
+            <div className={styles.locDotWrapper}>
+              <MiddleDot className={styles.middleDot} />
+            </div>
+            <div className={styles.detailLocWrapper}>
+              <span className={styles.detailLoc}>304호 첫 번째 줄 책상 위</span>
+            </div>
+          </div>
           <div className={styles.contentWrapper}>
-            <span className={styles.content}>{post?.content}</span>
+            {/* post?.content.split("\n").map((line) => {
+              return <p className={styles.line}>{line}</p>;
+            }) */}
+            <p className={styles.line}>버티컬 마우스 찾아가세요.</p>
+            <p className={styles.line}>
+              강의실 201호 맨 앞 자리에 그대로 뒀습니다.
+            </p>
           </div>
         </div>
       </div>
       <div className={styles.commentContainer}>
         <div className={styles.commentHelpContainer}>
           <div className={styles.commentHelpWrapper}>
-            <span className={styles.commentHelp}>
+            <p className={styles.commentHelp}>
               분실물 보관처에 물건을 맡기셨다면,
-              <br />
+            </p>
+            <p className={styles.commentHelp}>
               분실물이 주인을 잘 찾아갈 수 있도록 댓글을 달아주세요!
-            </span>
+            </p>
           </div>
         </div>
-        <div className={styles.commmentListContainer}></div>
+        <div className={styles.commmentListContainer}>
+          <Comment />
+        </div>
       </div>
-      <div className={styles.leaveCommentContainer}></div>
+      <div className={styles.bottomBar}>
+        <div
+          className={`${styles.completeButtonContainer} ${styles.notCompleted}`}
+        >
+          {/* <div
+          className={`${styles.completeButtonContainer} ${
+            post?.status === "false" ? styles.notCompleted : styles.completed
+          }`}
+        > */}
+          <div className={styles.completedIconWrapper}>
+            <Complete className={styles.completedIcon} />
+          </div>
+          <div className={styles.completedTextWrapper}>
+            <span className={styles.completedText}>수령 완료</span>
+          </div>
+        </div>
+        <div className={styles.leaveCommentContainer}>
+          <div className={styles.commentInputWrapper}>
+            <input
+              type="text"
+              placeholder="댓글을 입력하세요."
+              className={styles.commentInput}
+            />
+          </div>
+          <div className={styles.commentSendButtonWrapper}>
+            <Send />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
