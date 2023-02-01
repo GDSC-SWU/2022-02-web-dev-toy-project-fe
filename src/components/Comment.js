e3aeae6e0430664b06bb605f4478014df849b519
Yusun Choi <nsuy.ch@gmail.com>
2023-02-02 03:04:26 +0900

    feat: Add parseDate on Comment (#20)

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,7 +1,8 @@
 import styles from "./Comment.module.css";
+import parseDate from "../util/hooks/parseDate";
 import { ReactComponent as Profile } from "../assets/images/postDetail/profile_none.svg";
 
-const Comment = () => {
+const Comment = ({ comment, num }) => {
   return (
     <div className={styles.container}>
       <div className={styles.writerContainer}>
@@ -9,15 +10,21 @@ const Comment = () => {
           <Profile />
         </div>
         <div className={styles.writerWrapper}>
-          <span className={styles.writer}>익명1</span>
+          <span className={styles.writer}>{/* `익명${num}` */}익명1</span>
         </div>
       </div>
       <div className={styles.contentWrapper}>
-        <p className={styles.line}>헉 혹시 오른쪽 세 번째 자리였나요?</p>
+        <p className={styles.line}>
+          {/* comment.content */}헉 혹시 오른쪽 세 번째 자리였나요?
+        </p>
       </div>
       <div className={styles.publishDateContainer}>
-        <span className={styles.publishDate}>01/18</span>
-        <span className={styles.publishDate}>20:41</span>
+        <span className={styles.publishDate}>
+          {/* parseDate(comment.commentDate, true) */}01/18
+        </span>
+        <span className={styles.publishDate}>
+          {/* parseDate(comment.commentDate, false) */}20:41
+        </span>
       </div>
       <div className={styles.border}></div>
     </div>
