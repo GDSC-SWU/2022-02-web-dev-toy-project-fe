5e7a2d5977bba7797078fd9527b8cfb1d5479c82
Yusun Choi <nsuy.ch@gmail.com>
2023-02-02 08:55:59 +0900

    feat: Remove sample data (#20)

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,5 +1,5 @@
 import styles from "./Comment.module.css";
-import parseDate from "../util/hooks/parseDate";
+import parseDate from "../util/parseDate";
 import { ReactComponent as Profile } from "../assets/images/postDetail/profile_none.svg";
 
 const Comment = ({ comment, num }) => {
@@ -10,20 +10,18 @@ const Comment = ({ comment, num }) => {
           <Profile />
         </div>
         <div className={styles.writerWrapper}>
-          <span className={styles.writer}>{/* `익명${num}` */}익명1</span>
+          <span className={styles.writer}>{`익명${num}`}</span>
         </div>
       </div>
       <div className={styles.contentWrapper}>
-        <p className={styles.line}>
-          {/* comment.content */}헉 혹시 오른쪽 세 번째 자리였나요?
-        </p>
+        <p className={styles.line}>{comment.content}</p>
       </div>
       <div className={styles.publishDateContainer}>
         <span className={styles.publishDate}>
-          {/* parseDate(comment.commentDate, true) */}01/18
+          {parseDate(comment.commentDate, true)}
         </span>
         <span className={styles.publishDate}>
-          {/* parseDate(comment.commentDate, false) */}20:41
+          {parseDate(comment.commentDate, false)}
         </span>
       </div>
       <div className={styles.border}></div>
