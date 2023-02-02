f268722be8ec252e3697025603bd25f07e339265
yeonsu <drawingbonny@gmail.com>
2023-02-02 14:20:21 +0900

    feat: Add webmail auth number validation (#19)

--- src/pages/ConfirmWebmailPage.js
+++ src/pages/ConfirmWebmailPage.js
@@ -1,19 +1,35 @@
 import React from "react";
 import styles from "./ConfirmWebmailPage.module.css";
 import { Link } from "react-router-dom";
-import { useState } from "react";
+import { useState, useCallback } from "react";
 import NavBar from "../components/UI/NavigationBar";
 
 function SignUp() {
   const [userAuthNumber, setuserAuthNumber] = useState("");
+  const [isAuthNumber, setIsAuthNumber] = useState(false);
+  const [AuthMessage, setAuthMessage] = useState("");
 
-  const handleUserAuthNumber = (e) => {
-    setuserAuthNumber(e.target.value);
-  };
+  // const handleUserAuthNumber = (e) => {
+  //   setuserAuthNumber(e.target.value);
+  // };
 
   // const createUser = async () => {
   //   const { data } = await axios.post(`auth`);
   // };
+
+  // 인증번호 유효성 검사
+  const onChangeAuthNumber = useCallback((e) => {
+    const authCurrent = e.target.value;
+    setuserAuthNumber(authCurrent);
+
+    if (e.target.value.length < 6) {
+      setAuthMessage("");
+      setIsAuthNumber(false);
+    } else {
+      setAuthMessage("");
+      setIsAuthNumber(true);
+    }
+  }, []);
 
   return (
     <>
@@ -32,21 +48,20 @@ function SignUp() {
                 className={styles.input_authNumber}
                 placeholder="인증번호입력(3분 이내)"
                 value={userAuthNumber}
-                onChange={handleUserAuthNumber}
+                onChange={onChangeAuthNumber}
               />
               <p className={styles.alert}>
                 ※ 이메일로 발송된 인증번호를 입력해주세요.
+                <span>{AuthMessage}</span>
               </p>
               {/* {errors.userNickname && <p>{errors.userNickname}</p>} */}
             </div>
             <Link
-              to={userAuthNumber ? "/welcome" : null}
+              to={isAuthNumber ? "/welcome" : null}
               className={
-                userAuthNumber.length
-                  ? styles.confirmBtn
-                  : styles.confirmBtnDisable
+                isAuthNumber ? styles.confirmBtn : styles.confirmBtnDisable
               }
-              state={userAuthNumber}
+              state={isAuthNumber}
             >
               학교 인증 완료하기
             </Link>
